f5df131fb28ffcd91a19b1b0ce118304d40b47d3
John Waweru <john.waweru@nathandigital.com>
2025-02-21 15:43:56 +0300

    configured client basics

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,6 +1,8 @@
 const { Signup, Login } = require("../controllers/AuthController");
+const { userVerification } = require("../middlewares/AuthMiddleware");
 const router = require("express").Router();
 
+router.post("/", userVerification);
 router.post("/signup", Signup);
 router.post("/login", Login);
 
